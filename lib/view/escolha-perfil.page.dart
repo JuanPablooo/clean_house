c71b263d88639d25b149ffc65da1b1975771e86f
JuanPablooo <juanpablopereiragomes@hotmail.com>
2020-05-25 15:37:25 -0300

    adcionando area da escolha do perfil

--- lib/view/escolha-perfil.page.dart
+++ lib/view/escolha-perfil.page.dart
@@ -1,3 +1,6 @@
+import 'package:clean_house/constants/cores.dart';
+import 'package:clean_house/view/widgets/background.dart';
+import 'package:clean_house/view/widgets/buttons/btn-generic.dart';
 import 'package:flutter/material.dart';
 
 class EscolhaPerfil extends StatelessWidget{
@@ -10,42 +13,26 @@ class EscolhaPerfil extends StatelessWidget{
           style: TextStyle() ,
         ),
       ),
+      resizeToAvoidBottomInset : false,
       body: Container(
-        color: Color.fromRGBO(255,255,255, 1),
-        padding: EdgeInsets.only(top: 20, left: 40, right: 40 ),
+        decoration: Background.desenha("background.png"),
+        padding: EdgeInsets.only(top: 20, left: 25, right: 25 ),
         child: ListView(
           children: <Widget>[
             SizedBox(
-              height: 20,
+              height: 35,
             ),
             Column(
 
               children: <Widget>[
                 Text(
-                  "Bem vindo(a) a Clean House",
+                  "Cadastre-se como:",
                   textAlign: TextAlign.center,
                   style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.black87,
+                    fontSize: 27,
+                    color: Colors.white,
                   ),
                 ),
-                SizedBox(
-                  height: 15,
-                ),
-                Container(
-                  child: SizedBox(
-                    child: Image.asset("assets/logo.png"),
-                    height: 65,
-                    width: 70,
-                  ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Text(
-                    "selecione o seu perfil de Cadastro",
-                  style: TextStyle(fontSize: 17, color: Colors.black87),
-                )
               ],
             ),
             SizedBox(
@@ -53,100 +40,139 @@ class EscolhaPerfil extends StatelessWidget{
             ),
             Container(
               child: SizedBox(
-                height: 200,
-                width: 331,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+                height: 305,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: <Widget>[
-                        SizedBox(
-                          width: 150,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: <Widget>[
-                              SizedBox(
-                                height: 120,
-                                width: 100,
-                                child: Image.asset("assets/funcionaria.png"),
-                              ),
-                              Container(
-                                height: 47,
-                                width: 130,
-                                decoration: BoxDecoration(
-                                  color: Color.fromRGBO(22,119,199,1.0),
-                                  borderRadius: BorderRadius.all(Radius.circular(5)),
-                                  boxShadow: [
-                                    BoxShadow(
-                                      color: Color.fromRGBO(0, 0, 0, 0.25),
-                                      offset: Offset(1, 3),
-                                      blurRadius: 1.5,
-                                      spreadRadius: 0.8,
-                                    )
-                                  ]
-                                ),
-                                child: FlatButton(onPressed: ()=>{},
-                                    child: Row(
-                                      mainAxisAlignment: MainAxisAlignment.center,
-                                      children: <Widget>[
-                                        Text(
-                                          "Profissional",
-                                          style: TextStyle(color: Colors.white),
-                                        )
-                                      ],
-                                    )),
-                              ),
-                            ],),
-                        ),
-                        SizedBox(
-                          width: 150,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: <Widget>[
-                              SizedBox(
-                                height: 120,
-                                width: 100,
-                                child: Image.asset("assets/cliente.png"),
-                              ),
-                              Container(
-                                height: 47,
-                                width: 130,
-                                decoration: BoxDecoration(
-                                  color: Color.fromRGBO(25,184,212,1),
-                                  boxShadow: [
-                                    BoxShadow(
-                                      color: Color.fromRGBO(0, 0, 0, 0.25),
-                                      offset: Offset(1, 3),
-                                      blurRadius: 1.5,
-                                      spreadRadius: 0.8,
-                                    )
-                                  ],
-                                  borderRadius: BorderRadius.all(Radius.circular(5))
-                                ),
-                                child: FlatButton(onPressed: ()=>{},
 
-                                    child: Row(
-                                      mainAxisAlignment: MainAxisAlignment.center,
-                                      children: <Widget>[
-                                        Text(
-                                          "Cliente",
-                                          style: TextStyle(
-                                            color: Colors.white
-                                          ),
-                                        )
-                                      ],
-                                    )),
-                              ),
-                            ],),
-                        ),
-
-                      ],
+                    SizedBox(
+                      height: 150,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          SizedBox(
+                            height: 100,
+                            width: 100,
+                            child: Image.asset("assets/funcionaria.png"),
+                          ),
+                          btnGeneric("PROFISSIONAL", myDarkBlue, null, null)
+                        ],
+                      )
+                    ),
+                    SizedBox(
+                        height: 150,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: <Widget>[
+                            SizedBox(
+                              height: 100,
+                              width: 100,
+                              child: Image.asset("assets/cliente.png"),
+                            ),
+                            btnGeneric("CLIENTE", myDarkBlue, null, null)
+                          ],
+                        )
                     ),
                   ],
                 ),
               ),
             ),
+//            Container(
+//              child: SizedBox(
+//                height: 200,
+//                width: 331,
+//                child: Column(
+//                  mainAxisAlignment: MainAxisAlignment.center,
+//                  children: <Widget>[
+//                    Row(
+//                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+//                      children: <Widget>[
+//                        SizedBox(
+//                          width: 150,
+//                          child: Column(
+//                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+//                            children: <Widget>[
+//                              SizedBox(
+//                                height: 120,
+//                                width: 100,
+//                                child: Image.asset("assets/funcionaria.png"),
+//                              ),
+//                              Container(
+//                                height: 47,
+//                                width: 130,
+//                                decoration: BoxDecoration(
+//                                  color: Color.fromRGBO(22,119,199,1.0),
+//                                  borderRadius: BorderRadius.all(Radius.circular(5)),
+//                                  boxShadow: [
+//                                    BoxShadow(
+//                                      color: Color.fromRGBO(0, 0, 0, 0.25),
+//                                      offset: Offset(1, 3),
+//                                      blurRadius: 1.5,
+//                                      spreadRadius: 0.8,
+//                                    )
+//                                  ]
+//                                ),
+//                                child: FlatButton(onPressed: ()=>{},
+//                                    child: Row(
+//                                      mainAxisAlignment: MainAxisAlignment.center,
+//                                      children: <Widget>[
+//                                        Text(
+//                                          "Profissional",
+//                                          style: TextStyle(color: Colors.white),
+//                                        )
+//                                      ],
+//                                    )),
+//                              ),
+//                            ],),
+//                        ),
+//                        SizedBox(
+//                          width: 150,
+//                          child: Column(
+//                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+//                            children: <Widget>[
+//                              SizedBox(
+//                                height: 120,
+//                                width: 100,
+//                                child: Image.asset("assets/cliente.png"),
+//                              ),
+//                              Container(
+//                                height: 47,
+//                                width: 130,
+//                                decoration: BoxDecoration(
+//                                  color: Color.fromRGBO(25,184,212,1),
+//                                  boxShadow: [
+//                                    BoxShadow(
+//                                      color: Color.fromRGBO(0, 0, 0, 0.25),
+//                                      offset: Offset(1, 3),
+//                                      blurRadius: 1.5,
+//                                      spreadRadius: 0.8,
+//                                    )
+//                                  ],
+//                                  borderRadius: BorderRadius.all(Radius.circular(5))
+//                                ),
+//                                child: FlatButton(onPressed: ()=>{},
+//
+//                                    child: Row(
+//                                      mainAxisAlignment: MainAxisAlignment.center,
+//                                      children: <Widget>[
+//                                        Text(
+//                                          "Cliente",
+//                                          style: TextStyle(
+//                                            color: Colors.white
+//                                          ),
+//                                        )
+//                                      ],
+//                                    )),
+//                              ),
+//                            ],),
+//                        ),
+//
+//                      ],
+//                    ),
+//                  ],
+//                ),
+//              ),
+//            ),
             SizedBox(
               height: 13,
             ),
--- lib/view/login.page.dart
+++ lib/view/login.page.dart
@@ -23,6 +23,7 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context){
     return Scaffold(
+      resizeToAvoidBottomInset : false,
 
       body: Container(
         decoration: Background.desenha("background.png"),
